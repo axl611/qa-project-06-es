eb06f6eb2a0550832e88e5ff2078e23e56863cda
axl611 <axl611@gmail.com>
2024-01-17 09:58:06 -0600

    project modified

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://fa1906b5-952e-49dd-a12f-b673f9e45cd5.serverhub.tripleten-services.com"
+URL_SERVICE = "https://8ebed682-9dab-4eff-8743-598ef8cb38a7.serverhub.tripleten-services.com"
 # Inserta tu dirección de URL sin la barra diagonal al final
 CREATE_USER_PATH = "/api/v1/users"
 # Almacena la ruta para crear un usuario o usuaria en esta variable
--- sender_stand_request.py
+++ sender_stand_request.py
@@ -3,32 +3,32 @@ import requests
 import data
 
 
-def post_new_user(body):  # crear nuevo usuario
+def create_new_user_request(body):  # crear nuevo usuario
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
                          json=body,
                          headers=data.headers)
 
-response = post_new_user(data.user_body)
-print(response.status_code)
+user_response = create_new_user_request(data.user_body)
+print(user_response.status_code)
 
-def get_token():  # obtener authtoken
-    response_data = response.json()
+def obtain_auth_token_from_response():  # obtener authtoken
+    response_data = user_response.json()
     return response_data['authToken']
 
-auth_token = get_token()
+auth_token = obtain_auth_token_from_response()
 
 # Imprimir el token
 print("authToken:", auth_token)
 
-def send_post_request(kit_body):
-    auth_token = get_token()
+def send_kit_creation_request(kit_body):
+    auth_token = obtain_auth_token_from_response()
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {auth_token}"
     }
-    response = requests.post(configuration.URL_SERVICE + configuration.KITS_PATH, headers=headers,
+    kit_response = requests.post(configuration.URL_SERVICE + configuration.KITS_PATH, headers=headers,
                              json=kit_body)
 
-    return response
+    return kit_response
 
-print(response.status_code)
+print(user_response.status_code)
